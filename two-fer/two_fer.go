d211fe1c05b6b6b1cb15a05fe20e194b945abbb8
url8m8 <59782098+url8m8@users.noreply.github.com>
2020-02-11 15:46:10 -0500

    Final twofer, next problem.

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -1,9 +1,9 @@
-// Implements the ShareWith function, returning a string with the name variable.
+// Package twofer implements the ShareWith function, returning a string with the name variable.
 package twofer
 
 import "fmt"
 
-// Takes a name string, returning a message.  If name string is empty, returns generic message.
+// ShareWith takes a name string, returning a message.  If name string is empty, returns generic message.
 func ShareWith(name string) string {
 	if name == "" {
 		return "One for you, one for me."
