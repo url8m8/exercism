d85e854c1354be3b495adf7a902a8ca7b76f17da
url8m8 <59782098+url8m8@users.noreply.github.com>
2020-02-18 14:45:08 -0500

    Final hamming solution

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,19 +1,19 @@
 // Package hamming calculates the hamming distance between two DNA strands.
 package hamming
 
-import "fmt"
+import "errors"
 
-// Distance checks to see if strands are equal, if so, a for loop compares the two and keeps account of difference in index variable.
+// Distance checks to see if strands are equal, if so, a for loop compares the two and keeps account of difference in distance variable.
 func Distance(a, b string) (int, error) {
-    var index int
-
-    if len(a) != len(b) {
-        return 0, fmt.Errorf("Strands must be of equal length.")
-    }
-    for i := 0; i < len(a); i++ {
-        if a[i] != b[i] {
-            index += 1
-        }
-    }
-    return index, nil
+	ar, br := []rune(a), []rune(b)
+	if len(ar) != len(br) {
+		return 0, errors.New("strands must be of equal length")
+	}
+	var distance int
+	for i := range ar {
+		if ar[i] != br[i] {
+			distance++
+		}
+	}
+	return distance, nil
 }
